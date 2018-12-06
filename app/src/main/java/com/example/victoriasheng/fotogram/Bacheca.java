24edaca4ce7f26a5551fa737cd7ef17c341309b1
Victoria <victoriasheng@hotmail.com>
2018-12-06 17:30:50 +0100

    Creazione unicamente delle activity vuote

---
+++ app/src/main/java/com/example/victoriasheng/fotogram/Bacheca.java
@@ -0,0 +1,13 @@
+package com.example.victoriasheng.fotogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bacheca extends AppCompatActivity {
+    //lista dei post in ordine di data e menu in fondo fisso
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bacheca);
+    }
+}
--- app/src/main/java/com/example/victoriasheng/fotogram/MainActivity.java
+++ app/src/main/java/com/example/victoriasheng/fotogram/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.victoriasheng.fotogram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // al click del bottone se il nome e la password sono corretti , vado alla bcheca
+        //non vuole il link allaregistrazione su server
+        Button buttonServer=findViewById(R.id.NameSearchOnServer);
+        
     }
 }
