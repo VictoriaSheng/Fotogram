f77fc3c9fb7f43102ae1857434b223f641d61a0a
fcirini <francy.cirini@gmail.com>
2018-12-15 13:59:17 +0100

    a

--- app/src/main/java/com/example/victoriasheng/fotogram/Bacheca.java
+++ app/src/main/java/com/example/victoriasheng/fotogram/Bacheca.java
@@ -1,11 +1,17 @@
 package com.example.victoriasheng.fotogram;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.lang.reflect.Field;
 
 public class Bacheca extends AppCompatActivity {
     //lista dei post in ordine di data e menu in fondo fisso
@@ -16,7 +22,8 @@ public class Bacheca extends AppCompatActivity {
         setContentView(R.layout.activity_bacheca);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.main_nav);
+  
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/victoriasheng/fotogram/MainActivity.java
+++ app/src/main/java/com/example/victoriasheng/fotogram/MainActivity.java
@@ -1,43 +1,40 @@
 package com.example.victoriasheng.fotogram;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.view.View;
-import com.android.volley.RequestQueue;
-import org.json.JSONObject;
-
-import com.android.volley.toolbox.JsonRequest;
-import com.android.volley.toolbox.Volley;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.NetworkResponse;
-import com.android.volley.AuthFailureError;
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
-
-import org.json.JSONException;
-import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.android.volley.toolbox.JsonObjectRequest;
-public class MainActivity extends AppCompatActivity {
 
-    RequestQueue requestQueue = Volley.newRequestQueue(this);
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // al click del bottone se il nome e la password sono corretti , vado alla bcheca
-        //non vuole il link allaregistrazione su server
         Button buttonServer=findViewById(R.id.NameSearchOnServer);
         buttonServer.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
@@ -45,59 +42,37 @@ public class MainActivity extends AppCompatActivity {
                 String password = edit.getText().toString();
                 EditText edit2 = (EditText)findViewById(R.id.nome);
                 String username = edit2.getText().toString();
-                String url = "https://ewserver.di.unimi.it/mobicomp/fotogram/login";
-                JSONObject postparams = new JSONObject();
-                try {
-                    postparams.put("username", username);
-                    postparams.put("password", password);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                final String result = "";
-                JsonObjectRequest request = new JsonObjectRequest(JsonRequest.Method.POST, url, postparams, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response){
-
-                        Log.d("Reponse", response.toString());
-
-                        EditText editText = (EditText)findViewById(R.id.nome);
-                        editText.setText(response.toString(), TextView.BufferType.EDITABLE);
-
-                        //startActivity(new Intent(MainActivity.this, Bacheca.class));
-
-                    }
-
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        volleyError.printStackTrace();
-                        Log.d("Error = ", volleyError.toString());
-                    }
-                }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        return null;
-                    }
-
-                };
-                requestQueue.add(request);
+                //queue = Volley.newRequestQueue(ctx);
+                getJsonResponsePost();
             }
         });
     }
 
 
+    public void  getJsonResponsePost(){
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        Map<String, String> params = new HashMap();
+        params.put("username", "feng");
+        params.put("password", "feng");
 
+        JSONObject parameters = new JSONObject(params);
+        String url = "https://ewserver.di.unimi.it/mobicomp/fotogram/login";
 
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        EditText editText = (EditText)findViewById(R.id.nome);
+                        editText.setText("aaa".toString(), TextView.BufferType.EDITABLE);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                EditText editText = (EditText)findViewById(R.id.nome);
+                editText.setText(error.toString(), TextView.BufferType.EDITABLE);
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
 
-
-
-
-
-
-
-//creo un bottone che mi porta alla bacheca per ora, una volta implementato il login si può togliere
-  /*  public void Bacheca(View v) {
-        Intent intent=new Intent(MainActivity.this,Bacheca.class);//creo l'intent per colleare la classe NuovaMatricola
-        startActivity(intent);
-    }*/
 }
